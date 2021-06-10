f2d82cdc73ee55b6a64bd24789f9ce258cb76bd1
Abdelrahman Mahmoud <60822166+abdelrahmman97@users.noreply.github.com>
2021-06-10 12:16:59 +0200

    Fix Error in RegisterDoctorActivity

--- app/src/main/java/com/android/aimedicalcare/RegisterDoctorActivity.kt
+++ app/src/main/java/com/android/aimedicalcare/RegisterDoctorActivity.kt
@@ -8,7 +8,7 @@ import android.view.View
 class RegisterDoctorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_patient)
+        setContentView(R.layout.activity_register_doctor)
 
         val btnSignIn = findViewById<View>(R.id.button4)
         val btnRgistr = findViewById<View>(R.id.button5)

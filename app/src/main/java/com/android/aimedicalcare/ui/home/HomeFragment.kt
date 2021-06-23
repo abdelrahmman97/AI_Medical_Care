10b3bf6125adc55fae86864d369b687e5c251f49
Abdelrahman Mahmoud <60822166+abdelrahmman97@users.noreply.github.com>
2021-06-23 21:27:13 +0200

    Added QR Code Scanner Added QR Code Generator Added Voice Recorder

--- app/src/main/java/com/android/aimedicalcare/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/aimedicalcare/ui/home/HomeFragment.kt
@@ -1,23 +1,25 @@
 package com.android.aimedicalcare.ui.home
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.android.aimedicalcare.DiagnoseActivity
 import com.android.aimedicalcare.MedicalHistoryActivity
 import com.android.aimedicalcare.TextDiagnoseActivity
 import com.android.aimedicalcare.databinding.FragmentHomeBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -48,6 +50,7 @@ class HomeFragment : Fragment() {
         goToDiagnose.setOnClickListener {
             val intent = Intent(activity, DiagnoseActivity::class.java)
             activity?.startActivity(intent)
+
         }
 
         goTpTextDiagnose.setOnClickListener {
@@ -60,12 +63,38 @@ class HomeFragment : Fragment() {
             activity?.startActivity(intent)
         }
 
+        scanQrCode.setOnClickListener {
+            val scan = IntentIntegrator.forSupportFragment(this)
+            scan.initiateScan()
+
+        }
+
         return root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode== Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(activity, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(activity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 
 }
